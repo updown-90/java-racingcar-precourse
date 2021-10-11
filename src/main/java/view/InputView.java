ed18d662ddea6293112364aa424305c8dc46e8e5
updown-90 <updown_90@kakao.com>
2021-10-11 22:36:28 +0900

    refactor: 사용자 입력 nextstep.utils에서 제공하는 클래스로 받도록 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,22 +1,22 @@
 package view;
 
+import nextstep.utils.Console;
+
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class InputView {
-	private static Scanner SCANNER = new Scanner(System.in);
 	public static final String SPLIT_SEPARATOR = ",";
 
 	public static List<String> scannerUserInputCarsNames() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
 
-		return Arrays.asList(SCANNER.next().split(SPLIT_SEPARATOR));
+		return Arrays.asList(Console.readLine().split(SPLIT_SEPARATOR));
 	}
 
 	public static int scannerUserInputRacingCount() {
 		System.out.println("시도할 회수는 몇회인가요 ?");
-		
-		return SCANNER.nextInt();
+
+		return Integer.parseInt(Console.readLine());
 	}
 }
